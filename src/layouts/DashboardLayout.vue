2ee46858c03e4a9334c3a9be69be9551ed8d4c12
Jorge Muñoz <jorge98mv@gmail.com>
2025-05-12 23:30:21 +0200

    handle main area visibility on small screens

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,17 +1,27 @@
 <script setup>
-  import SidebarElement from '@/components/elements/SidebarElement.vue'
-  import TopbarElement from '@/components/elements/TopbarElement.vue'
+import { ref } from 'vue'
+import SidebarElement from '@/components/elements/SidebarElement.vue'
+import TopbarElement from '@/components/elements/TopbarElement.vue'
+import { useDevice } from '@/composables/useDevice'
+
+const dynamicClass = ref('')
+const { isMobile, isTablet } = useDevice()
+
+const handleVisibility = (menuOpen) => {
+  if (isMobile.value || isTablet.value) {
+    menuOpen ? (dynamicClass.value = 'overflow-hidden blur-sm') : (dynamicClass.value = '')
+  }
+}
 </script>
 
 <template>
   <div class="flex h-dvh bg-light text-text">
     <!-- Sidebar -->
-    <SidebarElement />
-
+    <SidebarElement @toggle-sidebar="handleVisibility" />
     <!-- Main Content -->
-    <div class="flex-1 flex-col">
+    <div :class="dynamicClass" class="flex-1 flex-col">
       <!-- Topbar -->
-      <TopbarElement></TopbarElement>
+      <TopbarElement />
       <!-- Content Slot -->
       <main class="p-6 overflow-y-auto flex-1 bg-light">
         <slot />
