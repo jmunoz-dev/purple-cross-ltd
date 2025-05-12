b09e1e12096ae9a129ddd9c9c35ce8d4d6b1ded0
Jorge Muñoz <jorge98mv@gmail.com>
2025-05-12 21:01:48 +0200

    add icon component

---
+++ src/components/blocks/IconBlock.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+
+defineProps({
+  name: { type: String, required: true },
+  customIconClass: { type: String, default: 'w-6 h-6' },
+})
+</script>
+
+<template>
+  <div class="icon-component">
+    <Icon :icon="name" :class="customIconClass" />
+  </div>
+</template>
