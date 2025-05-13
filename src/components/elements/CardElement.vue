7fae80f20761ddb3a6c45625df4a89147801c697
Jorge Muñoz <jorge98mv@gmail.com>
2025-05-13 11:06:33 +0200

    add card element and sytles

---
+++ src/components/elements/CardElement.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  type: { type: String, default: '' }
+})
+</script>
+
+<template>
+  <div class="card-component">
+    <div class="card-elements" :class="type">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="#scss/components/card.scss" />
