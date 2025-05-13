058999d0d6b4e658ce945a4664bffed1dc5f47a3
Jorge Muñoz <jorge98mv@gmail.com>
2025-05-13 16:56:16 +0200

    added error component to show messages

---
+++ src/components/controls/partials/ErrorComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  errors: { type: [String, Array, Boolean], default: false },
+  translate: { type: Boolean, default: false },
+})
+</script>
+
+<template>
+  <div class="error-component">
+    <div class="text-xs text-functional-danger">
+      <div v-if="Array.isArray(errors)" class="flex flex-col gap-2 min-h-4">
+        <span v-for="(error, index) in errors" :key="index">
+          {{ error ? error : '' }}
+        </span>
+      </div>
+      <span v-else class="block min-h-4">
+        {{ errors ? errors : '' }}
+      </span>
+    </div>
+  </div>
+</template>
