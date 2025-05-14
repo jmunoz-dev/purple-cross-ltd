a5eae007a57fffc7c977d2310c15fa891b1988e0
Jorge Muñoz <jorge98mv@gmail.com>
2025-05-14 16:46:05 +0200

    external functions to custom rendered data

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -15,3 +15,19 @@ export const addEmployeeAction = (name, occupation, department, dateOfEmployment
   department.value = ''
   dateOfEmployment.value = ''
 }
+
+export const getEmploymentStatus = (date) => {
+  if (!date) return ''
+  const now = new Date()
+  const employmentDate = new Date(date)
+
+  return employmentDate > now ? `Employed soon: ${date}` : `Currently employed: ${date}`
+}
+
+export const getTerminationStatus = (date) => {
+  if (!date) return ''
+  const now = new Date()
+  const terminationDate = new Date(date)
+
+  return terminationDate > now ? `To be terminated: ${date}` : `Terminated: ${date}`
+}
